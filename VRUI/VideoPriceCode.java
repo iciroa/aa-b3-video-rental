03aea8b40963ba480f1fc60eac0d484058168229
SD <sangdo913@naver.com>
2022-07-13 17:03:42 +0900

    VideoPriceCode.Regular.getRentCharge 부분 charge double로 변경

--- VRUI/VideoPriceCode.java
+++ VRUI/VideoPriceCode.java
@@ -1,9 +1,8 @@
 package VRUI;
-
 public enum VideoPriceCode {
     REGULAR {
         double getRentCharge (int daysRented) {
-            int charge = 2;
+            double charge = 2;
             if (daysRented > 2) charge += (daysRented-2)*1.5;
             return charge;
         }
